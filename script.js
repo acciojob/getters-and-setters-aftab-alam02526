951bd7b984b9d9693e818585656c667614ceaef3
aftab-alam02526 <aftab0447@gmail.com>
2025-09-18 14:01:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-//complete this code
+// Complete this code
 class Person {
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
+	constructor(name, age) {
+		this._name = name;  // Use _name
+		this._age = age;    // Use _age
 	}
 
+	// Getter for name
 	get newName() {
-		return this.name;
+		return this._name;
 	}
 
+	// Setter for age
 	set newAge(age) {
-		return this.age = age;
+		this._age = age;
 	}
 }
 
 class Student extends Person {
-	study(){
-	console.log( this.name + " is studying");
+	study() {
+		console.log(this._name + " is studying");
 	}
 }
 
 class Teacher extends Person {
-	teach(){
-	console.log( this.name + " is teaching");
+	teach() {
+		console.log(this._name + " is teaching");
 	}
 }
 
-const person = new Person ("John", 25);
-console.log(person.name); 
+// Testing Person
+const person = new Person("John", 25);
+console.log(person._name); // John
 
-person.age = 30;
-console.log(person.age);
+person.newAge = 30; // Using setter
+console.log(person._age); // 30
 
+// Testing Student
 const student = new Student("Alice", 22);
-student.study(); 
+student.study(); // Alice is studying
 
-
+// Testing Teacher
 const teacher = new Teacher("Bob", 40);
-teacher.teach();
+teacher.teach(); // Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
