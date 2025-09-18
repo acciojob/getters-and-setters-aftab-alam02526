f3f3158a4d9671ef674b6a9b230b1e4fdeefae00
aftab-alam02526 <aftab0447@gmail.com>
2025-09-18 14:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
 //complete this code
 class Person {
 	constructor(name, age){
-		this.name = name;
-		this.age = age;
+		this._name = name;  // Use _name to match test
+		this._age = age;    // Use _age to match test
 	}
 
+	// Getter for name
 	get newName() {
-		return this.name;
+		return this._name;
 	}
 
+	// Setter for age
 	set newAge(age) {
-		 this.age = age;
+		this._age = age;
+	}
+
+	// Getter for age (optional, but useful)
+	get age() {
+		return this._age;
+	}
+
+	// Setter for age (alternate access using direct property)
+	set age(value) {
+		this._age = value;
 	}
 }
 
 class Student extends Person {
-	study(){
-	console.log( this.name + " is studying");
+	study() {
+		console.log(this._name + " is studying");
 	}
 }
 
 class Teacher extends Person {
-	teach(){
-	console.log( this.name + " is teaching");
+	teach() {
+		console.log(this._name + " is teaching");
 	}
 }
 
 // Creating a Person instance
 const person = new Person("John", 25);
-console.log(person.name);  // Output: John
+console.log(person._name);  // Output: John
 
 person.age = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
+console.log(person._age);   // Output: 30
 
 // Creating a Student instance
 const student = new Student("Alice", 22);
@@ -45,3 +57,4 @@ teacher.teach();  // Output: Bob is teaching
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
